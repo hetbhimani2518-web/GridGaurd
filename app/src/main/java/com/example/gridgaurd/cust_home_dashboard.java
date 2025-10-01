e2d237118307e0058781171003897a26c40faf2c
hetbhimani2518-web <hetbhimani2518@gmail.com>
2025-10-01 14:53:31 +0530

    user home page

--- app/src/main/java/com/example/gridgaurd/cust_home_dashboard.java
+++ app/src/main/java/com/example/gridgaurd/cust_home_dashboard.java
@@ -8,13 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class cust_home_dashboard extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cust_home_dashboard);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            if (scrollY > oldScrollY) {
+                bottomNav.animate().translationY(bottomNav.getHeight()).setDuration(200); // Hide
+            } else {
+                bottomNav.animate().translationY(0).setDuration(200); // Show
+            }
+        });
 
     }
 }
